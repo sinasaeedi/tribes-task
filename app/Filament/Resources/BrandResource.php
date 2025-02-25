dcb3bb8267600afc373de20edd0ab5b5608bc19d
Sina Saeedi <saeedi.sina@gmail.com>
2025-02-25 15:16:22 +0330

    Add Filament ProductsRelationManager

--- app/Filament/Resources/BrandResource.php
+++ app/Filament/Resources/BrandResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\BrandResource\Pages;
 use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Filament\Resources\BrandResource\RelationManagers\ProductsRelationManager;
 use App\Models\Brand;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -83,7 +84,7 @@ class BrandResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            ProductsRelationManager::class,
         ];
     }
 
