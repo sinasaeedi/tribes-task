6d4232faedec55a3288515783504cdcdb0dc4046
Sina Saeedi <saeedi.sina@gmail.com>
2025-02-25 13:55:08 +0330

    Add Filament table default settings to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Tables;
+use Filament\Tables\Table;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Default table settings
+        Table::configureUsing(function (Table $table): void {
+            $table
+                ->searchOnBlur()
+                ->striped()
+                ->defaultSort('id', 'desc')
+                ->filters([
+                    Tables\Filters\TrashedFilter::make(),
+                ])
+                ->actions([
+                    Tables\Actions\ActionGroup::make([
+                        Tables\Actions\EditAction::make(),
+                        Tables\Actions\DeleteAction::make(),
+                    ]),
+                ])
+                ->bulkActions([
+                    Tables\Actions\BulkActionGroup::make([
+                        Tables\Actions\DeleteBulkAction::make(),
+                        Tables\Actions\ForceDeleteBulkAction::make(),
+                        Tables\Actions\RestoreBulkAction::make(),
+                    ]),
+                ]);
+        });
     }
 }
